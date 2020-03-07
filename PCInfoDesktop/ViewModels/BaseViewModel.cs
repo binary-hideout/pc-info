c508a911cbb027d3286db5b6b62d150c82a37603
netotz <neto.otz@hotmail.com>
2020-03-07 15:09:45 -0600

    Add base view-model
    This class will be used as parent for every other view-model

---
+++ PCInfoDesktop/ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PCInfoDesktop.ViewModels {
+    /// <summary>
+    /// Base view-model that the rest of view-models will inherit.
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged {
+
+        protected BaseViewModel() { }
+
+        /// <summary>
+        /// Event raised when a property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Method to apply to every property of a child view-model, e.g. 
+        /// <code>public var SomeProperty {get => _someProperty; set { _someProperty = value; OnPropertyChanged() }</code>
+        /// </summary>
+        /// <param name="propertyName">The property's name. It is unnecessary to specify as the name is automatically used.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(propertyName)
+            );
+        }
+    }
+}
