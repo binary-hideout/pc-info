e0c55a4476a626613c671478c827185588a73de5
netotz <neto.otz@hotmail.com>
2020-03-08 16:08:14 -0600

    Print installed apps' properties
    The properties printed are the same as listed in control panel

--- PCInfoDesktop/Models/Software.cs
+++ PCInfoDesktop/Models/Software.cs
@@ -17,9 +17,15 @@ namespace PCInfoDesktop.Models {
                 foreach (var subKeyName in key.GetSubKeyNames()) {
                     using (var tempKey = key.OpenSubKey(subKeyName)) {
                         // list available values to get
-                        foreach (var name in tempKey.GetValueNames()) {
-                            Console.WriteLine(name);
-                        }
+                        //foreach (var name in tempKey.GetValueNames()) {
+                        //    Console.WriteLine(name);
+                        //}
+                        var name = tempKey.GetValue("DisplayName");
+                        var publisher = tempKey.GetValue("Publisher");
+                        var date = tempKey.GetValue("InstallDate");
+                        var size = tempKey.GetValue("EstimatedSize");
+                        var version = tempKey.GetValue("DisplayVersion");
+                        Console.WriteLine($"{name}, {publisher}, {date}, {size}, {version}");
                     }
                 }
             }
