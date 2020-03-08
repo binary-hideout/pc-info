9a04c175109e91d22122e49df5edb8b1261a0540
netotz <neto.otz@hotmail.com>
2020-03-08 16:46:03 -0600

    Add class to store installed app's data

---
+++ PCInfoDesktop/Models/InstalledApplication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCInfoDesktop.Models {
+    /// <summary>
+    /// Data of an installed application (software) in the PC.
+    /// </summary>
+    public class InstalledApplication {
+        /// <summary>
+        /// Name of the application.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Publisher that installed the application.
+        /// </summary>
+        public string Publisher { get; set; }
+
+        /// <summary>
+        /// Date when the application was installed. It can be <c>null</c>, meaning that the date is unknown.
+        /// </summary>
+        public DateTime? InstallDate { get; set; }
+
+        /// <summary>
+        /// Estimated size in disk of the application.
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Current version of the application.
+        /// </summary>
+        public string Version { get; set; }
+
+    }
+}
