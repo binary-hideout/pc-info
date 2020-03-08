d2ea311063c2f5064c1144f1b2bc588dac155643
netotz <neto.otz@hotmail.com>
2020-03-08 13:18:46 -0600

    Add class to handle installed software

---
+++ PCInfoDesktop/Models/Software.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCInfoDesktop.Models {
+    /// <summary>
+    /// Handles the installed software in the PC.
+    /// </summary>
+    public class Software {
+
+    }
+}
