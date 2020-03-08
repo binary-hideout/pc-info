5f3e1752c7d68fd7e684f6976a3eb8503e73b91a
netotz <neto.otz@hotmail.com>
2020-03-08 13:37:02 -0600

    Add method to get installed applications
    Currently it just print the applications' names without storing them.

--- PCInfoDesktop/Models/Software.cs
+++ PCInfoDesktop/Models/Software.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +8,20 @@ namespace PCInfoDesktop.Models {
     /// Handles the installed software in the PC.
     /// </summary>
     public class Software {
+        /// <summary>
+        /// Gets the installed applications in the PC.
+        /// </summary>
+        public static void GetInstalledApps() {
+            string registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (var key = Registry.LocalMachine.OpenSubKey(registryKey)) {
+                foreach (var subkeyName in key.GetSubKeyNames()) {
+                    using (var subkey = key.OpenSubKey(subkeyName)) {
+                        var appName = subkey.GetValue("DisplayName");
+                        Console.WriteLine(appName);
+                    }
+                }
+            }
+        }
 
     }
 }
