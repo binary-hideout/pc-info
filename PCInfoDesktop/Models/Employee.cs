1a79b9aef5176d75d989ed29503b2e05dba852a0
DanielFl0 <MachoAlph@outlook.com>
2020-03-07 19:28:41 -0600

    Add Employee class

---
+++ PCInfoDesktop/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCInfoDesktop.Models
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private string firstLastName;
+        private string secondLastName;
+
+
+        public Employee(int id, string name, string firstLastName, string secondLastName)
+        {
+            this.id = id;
+            this.name = name;
+            this.firstLastName = firstLastName;
+            this.secondLastName = secondLastName;
+        }
+
+    }
+}
