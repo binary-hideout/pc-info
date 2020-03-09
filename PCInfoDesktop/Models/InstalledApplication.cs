e995ec04669c293d50039eb0d927926d9928a0b1
netotz <neto.otz@hotmail.com>
2020-03-09 14:08:54 -0600

    InstallDate property now doesn't support null
    In case the date is unknown, the property will be set to default (DateTime.MinValue).

--- PCInfoDesktop/Models/InstalledApplication.cs
+++ PCInfoDesktop/Models/InstalledApplication.cs
@@ -19,9 +19,9 @@ namespace PCInfoDesktop.Models {
         public string Publisher { get; set; }
 
         /// <summary>
-        /// Date when the application was installed. It can be <c>null</c>, meaning that the date is unknown.
+        /// Date when the application was installed. It can be <c>default</c> (<c>DateTime.MinValue</c>), meaning that the date is unknown.
         /// </summary>
-        public DateTime? InstallDate { get; set; }
+        public DateTime InstallDate { get; set; }
 
         /// <summary>
         /// Estimated size in disk of the application.
@@ -34,7 +34,7 @@ namespace PCInfoDesktop.Models {
         public string Version { get; set; }
 
         /// <summary>
-        /// Constructs an installed application by raw input (of type <c>object</c>) from <c>Software</c> class' method. If an argument is <c>null</c>, the corresponding property will be set as an empty string.
+        /// Constructs an installed application by raw input (of type <c>object</c>) from <c>Software</c> class' method. If an argument is <c>null</c>, the corresponding property will be set as an empty string, or <c>default</c> in the case of the date.
         /// </summary>
         /// <param name="name">Name of the application.</param>
         /// <param name="publisher">Publisher that installed the app.</param>
@@ -45,7 +45,7 @@ namespace PCInfoDesktop.Models {
         public InstalledApplication(object name, object publisher, object installDate, object size, object version) {
             Name = name is null ? string.Empty : name.ToString();
             Publisher = publisher is null ? string.Empty : publisher.ToString();
-            InstallDate = installDate is null ? DateTime.MinValue : ParseRawDate(installDate.ToString());
+            InstallDate = installDate is null ? default : ParseRawDate(installDate.ToString());
             Size = size is null ? 0 : Convert.ToInt32(size);
             Version = version is null ? string.Empty : version.ToString();
         }
@@ -54,15 +54,15 @@ namespace PCInfoDesktop.Models {
         /// Parses raw date format <c>yyyyMMdd</c> to a valid <c>DateTime</c> object.
         /// </summary>
         /// <param name="rawFormat"><c>string</c> representing a date in format <c>yyyyMMdd</c>.</param>
-        /// <returns><c>DateTime</c> if the <c>rawFormat</c> is not empty, else <c>null</c>.</returns>
-        private DateTime? ParseRawDate(string rawFormat) {
+        /// <returns><c>DateTime</c> if the <c>rawFormat</c> is not empty, else <c>default</c>.</returns>
+        private DateTime ParseRawDate(string rawFormat) {
             try {
                 var date = DateTime.ParseExact(rawFormat, "yyyyMMdd", CultureInfo.InvariantCulture);
                 return date;
             }
             // if the string argument is empty, an exception will be raised and the method will return null
             catch (FormatException) {
-                return null;
+                return default;
             }
         }
 
@@ -71,7 +71,7 @@ namespace PCInfoDesktop.Models {
         /// </summary>
         /// <returns><c>string</c> representing the application.</returns>
         public override string ToString() {
-            return $"{Name}, {Publisher}, {InstallDate}, {Size}, {Version}";
+            return $"{Name}, {Publisher}, {InstallDate.ToShortDateString()}, {Size}, {Version}";
         }
     }
 }
