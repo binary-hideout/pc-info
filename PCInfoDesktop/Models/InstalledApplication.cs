7d5cc6db4c4c2fb89e4cbc76eb3f3c8cd5b3d3e4
netotz <neto.otz@hotmail.com>
2020-03-09 11:50:41 -0600

    Add method to parse install date

--- PCInfoDesktop/Models/InstalledApplication.cs
+++ PCInfoDesktop/Models/InstalledApplication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace PCInfoDesktop.Models {
@@ -32,5 +33,21 @@ namespace PCInfoDesktop.Models {
         /// </summary>
         public string Version { get; set; }
 
+        /// <summary>
+        /// Parses raw date format <c>yyyyMMdd</c> to a valid <c>DateTime</c> object.
+        /// </summary>
+        /// <param name="rawFormat"><c>string</c> representing a date in format <c>yyyyMMdd</c>.</param>
+        /// <returns><c>DateTime</c> if the <c>rawFormat</c> is not empty, else <c>null</c>.</returns>
+        private DateTime? ParseRawDate(string rawFormat) {
+            try {
+                var date = DateTime.ParseExact(rawFormat, "yyyyMMdd", CultureInfo.InvariantCulture);
+                return date;
+            }
+            // if the string argument is empty, an exception will be raised and the method will return null
+            catch (FormatException) {
+                return null;
+            }
+
+        }
     }
 }
