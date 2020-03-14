2f29a53807259c9c147f978a5800714ecdf16615
netotz <neto.otz@hotmail.com>
2020-03-14 12:25:46 -0600

    Add class to store system information

---
+++ PCInfoDesktop/Models/SysInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCInfoDesktop.Models {
+    /// <summary>
+    /// Class to store system information: PC name, serial number and installed applications (software).
+    /// </summary>
+    public class SysInfo {
+
+    }
+}
