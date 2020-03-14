52a1e907c889977d1a301c0e41bb6ffa5481ed79
netotz <neto.otz@hotmail.com>
2020-03-14 12:28:38 -0600

    Add system properties

--- PCInfoDesktop/Models/SysInfo.cs
+++ PCInfoDesktop/Models/SysInfo.cs
@@ -7,6 +7,19 @@ namespace PCInfoDesktop.Models {
     /// Class to store system information: PC name, serial number and installed applications (software).
     /// </summary>
     public class SysInfo {
+        /// <summary>
+        /// PC name.
+        /// </summary>
+        public string Name { get; private set; }
 
+        /// <summary>
+        /// PC serial number.
+        /// </summary>
+        public string SerialNumber { get; private set; }
+
+        /// <summary>
+        /// Applications (software) that are installed in the PC.
+        /// </summary>
+        public List<InstalledApplication> InstalledApplications { get; private set; }
     }
 }
