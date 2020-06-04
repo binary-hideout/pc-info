2e0042941654aa894df7de1c856144dbfb914d7e
DanielFl0 <MachoAlph@outlook.com>
2020-06-04 17:00:07 -0500

    Add web host@

---
+++ pc-info-host/PC_INFO_HOST/Models/ReportSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_INFO_HOST.Models
+{
+    public class ReportSearch
+    {
+
+        public List<string> GetReports()
+        {
+            List<string> filesReport = new List<string> { };
+            string filePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string targetDirectory = System.IO.Path.GetDirectoryName(filePath);
+            //string netcoreapp = System.IO.Directory.GetParent(filePath).FullName;
+            //string debugDirectory = System.IO.Directory.GetParent(netcoreapp).FullName;
+            //string binDirectory = System.IO.Directory.GetParent(debugDirectory).FullName;
+            //string targetDirectory = System.IO.Directory.GetParent(binDirectory).FullName;
+            string[] paths = { targetDirectory, "wwwroot" };
+            string wwwrootPath = Path.Combine(paths);
+            string[] otherPaths = { wwwrootPath, "Reports" };
+            string reportsPath = Path.Combine(otherPaths);
+            if (!Directory.Exists(reportsPath))
+            {
+                Directory.CreateDirectory(reportsPath);
+            }
+            string[] subdirectoryEntries = Directory.GetFiles(reportsPath);
+            foreach (string file in subdirectoryEntries)
+            {
+                string correctPath = "~/Reports/" + Path.GetFileName(file);
+                filesReport.Add(correctPath);
+            }
+            return filesReport;
+        }
+
+    }
+}
---
+++ pc-info-host/PC_INFO_HOST/Pages/Reports.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PC_INFO_HOST.Models;
+
+namespace PC_INFO_HOST.Pages
+{
+    public class ReportsModel : PageModel
+    {
+        ReportSearch rs = new ReportSearch();
+        public List<string> Path { get; private set; }
+
+        public void OnGet()
+        {
+            Path = rs.GetReports();
+        }
+    }
+}
